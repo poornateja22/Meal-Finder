d19e5c4b9a4e962aa4445c0ac13ca2340e363812
poornateja <poornateja2002@gmail.com>
2024-09-07 15:16:45 +0530

    initial files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('menu-btn').addEventListener('click', function () {
+    document.getElementById('side-menu').style.width = '250px';
+});
+
+document.getElementById('close-btn').addEventListener('click', function () {
+    document.getElementById('side-menu').style.width = '0';
+});
