67310088587174ea8b1daa66a9252706818e9677
poornateja <poornateja2002@gmail.com>
2024-09-08 20:01:08 +0530

    added categories

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ document.getElementById('menu-btn').addEventListener('click', function () {
 document.getElementById('close-btn').addEventListener('click', function () {
     document.getElementById('side-menu').style.width = '0';
 });
+
+let img = document.getElementsByClassName('img');
+fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then(response => response.json())
+    .then(data => {
+        let categories = data.categories;
+        for (let i = 0; i < img.length && i < categories.length; i++) {
+            img[i].setAttribute('src', categories[i].strCategoryThumb);
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
